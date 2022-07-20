1bc728301944e608d9d1a2fd46f5f77197916141
longlife2049 <108611842+longlife2049@users.noreply.github.com>
2022-07-21 00:44:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   AUTHOR: '宾果数码', // 作者
   BIO: '宾果供应链', // 作者简介
   LINK: 'www.3hwork.com', // 网站地址
-  KEYWORDS: '宾果供应链,华强北', // 网站关键词 英文逗号隔开
+  KEYWORDS: '宾果供应链,华强北,一件代发', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '65552556acd148c8809ee164a92eec25', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
